fc2cb5678ddf3e4e3ac39535e77221f0c2f60ec6
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-11-12 17:42:16 +0800

    Add menu owo

--- dictionary-contactListing.py
+++ dictionary-contactListing.py
@@ -1,4 +1,7 @@
 #Menu
+print("<==========> Menu <==========> \n\n 1 -> Add item \n 2 -> Search \n 3 -> Exit (y/n) \n")
+print("<============================>")
+
 #user input
 #if Option 1
 #if Option 2
