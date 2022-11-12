20ab4f43d8b8387ac1bebdf66021a2c717a408f7
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-11-12 17:45:56 +0800

    user input done

--- dictionary-contactListing.py
+++ dictionary-contactListing.py
@@ -3,6 +3,9 @@ print("<==========> Menu <==========> \n\n 1 -> Add item \n 2 -> Search \n 3 -> 
 print("<============================>")
 
 #user input
+user_input = int(input("What do you want to do?(1-3)?: "))
+
+
 #if Option 1
 #if Option 2
 #if Option 3
