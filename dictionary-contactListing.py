847d2db0624f4a406d4c7e78331e25491d81beec
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-11-12 18:57:09 +0800

    added user input with dictionary

--- dictionary-contactListing.py
+++ dictionary-contactListing.py
@@ -3,9 +3,33 @@ print("<==========> Menu <==========> \n\n 1 -> Add item \n 2 -> Search \n 3 -> 
 print("<============================>")
 
 #user input
-user_input = int(input("What do you want to do?(1-3)?: "))
+user_input = int(input("\nWhat do you want to do?(1-3)?: "))
 
 
 #if Option 1
+if user_input == 1:
+    userList = {}
+    
+    #User Info with user input
+    print("\n<==========>Fill up the given information<==========>\n")
+    userName = input("Full Name: ")
+    userAge = input("Age: ")
+    userEmail = input("Email: ")
+    userAddress = input("Address: ")
+    userPhone = input("Phone Number: ")
+    print("\nThe user is now in the directory.\n")
+    
+    user = {
+        userName : {
+            "name" : userName,
+            "age" : userAge,
+            "email" : userEmail,
+            "address" : userAddress,
+            "phone" : userPhone 
+        }
+    } 
+    
+    print(user)
+    
 #if Option 2
 #if Option 3
